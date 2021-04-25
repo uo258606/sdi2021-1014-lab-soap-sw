7f738aecd942c75ba7339131dd261ec5e29d0317
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-04-26 00:53:53 +0200

    SDI-2021-1014-SW-11.2- Servicio SOAP Cliente

--- sdi2021-1014-lab-soap-cliente/src/main/java/com/uniovi/wsdl/package-info.java
+++ sdi2021-1014-lab-soap-cliente/src/main/java/com/uniovi/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.04.26 a las 12:52:47 AM CEST 
+// Generado el: 2021.04.26 a las 12:53:11 AM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/soap/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
